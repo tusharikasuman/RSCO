9e7fc19e5bf7a0d4c05a19da9b4334a777de4797
tusharikasuman <tusharika.suman6@gmail.com>
2025-05-28 23:47:42 +0530

    Script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//Navbar
+  document.addEventListener("DOMContentLoaded", function () {
+    const currentPath = window.location.pathname.split("/").pop() || "index.html";
+    const navLinks = document.querySelectorAll(".nav-links a");
+
+    navLinks.forEach(link => {
+      const href = link.getAttribute("href");
+      if (href === currentPath) {
+        link.classList.add("active");
+      }
+    });
+  });
+
+
 
 //Founders Section---
 
